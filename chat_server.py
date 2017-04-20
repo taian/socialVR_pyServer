acee195b97790d5e742f0f29215ed64d52926864
EC2 Default User <ec2-user@ip-172-31-11-102.ec2.internal>
2017-04-20 19:23:55 +0000

    updated server logic

--- chat_server.py
+++ chat_server.py
@@ -3,14 +3,14 @@ import sys
 import json
 
 # Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the socket to the address given on the command line
 server_name = sys.argv[1]
 server_address = (server_name, 10000)
 print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
-sock.listen(1)
+#sock.listen(1)
 
 # 6 users, 10 rooms total
 v3,w,h = 3, 6, 10
@@ -21,16 +21,17 @@ dataStr = ""
 
 while True:
     print >>sys.stderr, 'waiting for a connection'
-    connection, client_address = sock.accept()
-    connection.settimeout(2)
+ #   connection, client_address = sock.accept()
+ #   connection.settimeout(2)
     try:
-        print >>sys.stderr, 'client connected:', client_address
+#        print >>sys.stderr, 'client connected:', client_address
         while True:
             try:
-                data = connection.recv(BUFFER)
+                data, addr = sock.recvfrom(BUFFER)
+ #               data = connection.recv(BUFFER)
                 dataStr += data
                 
-                print >>sys.stderr, 'received "%s"' % data
+                print >>sys.stderr, 'received "%s"' % data, 'from', addr
                 if data:
                     #connection.sendall(data)
                     if len(data) < BUFFER:#this could be buggy, if the size happens to equal to buffer size
@@ -52,19 +53,25 @@ while True:
 
                                 rot = str(mat_rot[aRoom])
                                 info = '{"rot":' + rot + '}'
-                                connection.sendall(info)
+                                #connection.sendall(info)
+                                sock.sendto(info, addr)
                                 dataStr = ""
                             else:
-                                connection.sendall('Invalid data')
+                                #connection.sendall('Invalid data')
+                                print 'Invalid data', dataStr
+                                sock.sendto('Invalid data', addr)
                                 dataStr = ""
                         except ValueError:
                             print 'malformatted data',dataStr
+                            sock.sendto('Malformatted json string', addr)
                             dataStr = ""
-                            break; 
+                           # break; 
                 else:
                     dataStr = ""
                     break
-            except socket.timeout:
+            except socket.timeout, socket.error:
                 break;
     finally:
-        connection.close()
+        #connection.close()
+        sock.close()
+        print 'close sock'
